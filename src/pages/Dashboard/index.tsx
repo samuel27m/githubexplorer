8127c1f83f3ec2bfc8ad6b0f52d08ae4ead4919a
Samuel Caçador <samuelmoreira27@gmail.com>
2020-09-04 23:15:05 +0100

    Style Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Explore repositories on Github</Title>
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositories on Github</Title>
+
+            <Form>
+                <input placeholder="Type the repository name" />
+                <button type="submit">Search</button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img src="https://github.com/samuel27m.png" alt="Samuel" />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>
+                            A declarative, efficient, and flexible JavaScript
+                            library for building user interfaces.
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img src="https://github.com/samuel27m.png" alt="Samuel" />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>
+                            A declarative, efficient, and flexible JavaScript
+                            library for building user interfaces.
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img src="https://github.com/samuel27m.png" alt="Samuel" />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>
+                            A declarative, efficient, and flexible JavaScript
+                            library for building user interfaces.
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
     );
-}
+};
 
 export default Dashboard;
